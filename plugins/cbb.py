9b6ae608ac8b535bcaa5d287593d7f445773b882
Noob Dev <kingceylegend@gmail.com>
2025-03-26 12:59:50 +0000

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingXProBot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Jishu Developer</a>",
+            text = f"<b>🤖 Mon Nom :</b> <a href='https://t.me/KnowAD_Bot'>「ꞢNØW AĐ」🥀 BOT</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Chaîne :</b> <a href='https://t.me/BotZFlix'>𝔹𝕠𝕥🇿𝗙𝗹𝗶𝘅 [ᥴ𝘳ꪖɀꪗ] 🤪</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>◡̈⃝ㅤ🇰ιηg¢єу</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
