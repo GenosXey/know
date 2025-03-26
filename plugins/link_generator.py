17e6d89c352624ad594191c4a4835ad3452f00c0
Noob Dev <kingceylegend@gmail.com>
2025-03-26 12:50:57 +0000

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -10,34 +10,34 @@ from helper_func import encode, get_message_id
 async def batch(client: Client, message: Message):
     while True:
         try:
-            first_message = await client.ask(text = "Forward The First Message From DB Channel (With Quotes)..\n\nOr Send The DB Channel Post Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            first_message = await client.ask(text = "Transférez le premier message du canal DB (Kingcey).\n\nOu envoyez le lien de la publication du canal DB.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         f_msg_id = await get_message_id(client, first_message)
         if f_msg_id:
             break
         else:
-            await first_message.reply("❌ Error\n\nThis Forwarded Post Is Not From My DB Channel Or This Link Is Not Taken From DB Channel", quote = True)
+            await first_message.reply("❌ Error\n\nCe message transféré ne provient pas de mon canal DB ou ce lien n'est pas pris du canal DB.", quote = True)
             continue
 
     while True:
         try:
-            second_message = await client.ask(text = "Forward The Last Message From DB Channel (With Quotes)..\n\nOr Send The DB Channel Post Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            second_message = await client.ask(text = "Transférez le dernier message du canal DB. (Que vous vouliez enregistré)..\n\nOu envoyez le lien de la publication du canal DB.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         s_msg_id = await get_message_id(client, second_message)
         if s_msg_id:
             break
         else:
-            await second_message.reply("❌ Error\n\nThis Forwarded Post Is Not From My DB Channel Or This Link Is Not Taken From DB Channel", quote = True)
+            await second_message.reply("❌ Error\n\nCe message transféré ne provient pas de mon canal DB ou ce lien n'est pas pris du canal DB", quote = True)
             continue
 
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here Is Your Link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 PArtager Le lien", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>Voici le lien des ou du fichier(s) enregistré</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
 
@@ -46,20 +46,20 @@ async def batch(client: Client, message: Message):
 async def link_generator(client: Client, message: Message):
     while True:
         try:
-            channel_message = await client.ask(text = "Forward Message From The DB Channel (With Quotes)..\n\nOr Send The DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            channel_message = await client.ask(text = "Transférez le dernier message du canal DB. (Que vous vouliez enregistré)..\n\nOu envoyez le lien de la publication du canal DB.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         msg_id = await get_message_id(client, channel_message)
         if msg_id:
             break
         else:
-            await channel_message.reply("❌ Error\n\nThis Forwarded Post Is Not From My DB Channel Or This Link Is Not Taken From DB Channel", quote = True)
+            await channel_message.reply("❌ Error\n\nCe message transféré ne provient pas de mon canal DB ou ce lien n'est pas pris du canal DB", quote = True)
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here Is Your Link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Partager le lien", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b>Voici le lien des ou du fichier(s) enregistré</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
 
